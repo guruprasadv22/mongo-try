a1698fa5cdf0d70d5639e5e7604cb46890c10910
guruprasadv22 <guruprasadv22@gmail.com>
2022-07-07 21:41:09 +0530

    commit

--- myfile.py
+++ myfile.py
@@ -1,1 +1,13 @@
-connection_string = '''mongodb+srv://techwithtim:<password>@mycluster.vpopu.mongodb.net/?retryWrites=true&w=majority'''
+from dotenv import load_dotenv, find_dotenv
+import os
+import pprint
+from pymongo import MongoClient
+load_dotenv(find_dotenv())
+
+password = os.environ.get("MONGODB_PWD")
+
+connection_string = f'mongodb+srv://techwithtim:{password}@mycluster.vpopu.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(connection_string)
+
+dbs = client
